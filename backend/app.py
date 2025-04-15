d28b38e7c0e96ee270def2bb498b2393919fc8d4
ashutoh-excel2011 <ashutoh.excel2011@gmail.com>
2025-04-16 01:09:58 +0530

    Updated port

--- backend/app.py
+++ backend/app.py
@@ -434,5 +434,5 @@ def download_index_components():
         return redirect('/')
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 80))
     app.run(host='0.0.0.0', port=port, debug=False)
